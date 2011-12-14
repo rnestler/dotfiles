fc599c6c36c51a793ac93363ed723501f91b72f9
Raphael Nestler <raphael.nestler@gmail.com>
2011-12-15 00:42:48 +0100

    Add newsbeuter configuration
    config folder behaves similar to the bin folder

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for file in `find ${PWD} -maxdepth 1 -not -name '.*' -not -name README -not -name create_symlinks.sh -not -name bin -type f`; do
+for file in `find ${PWD} -maxdepth 1 -not -name '.*' -not -name README -not -name create_symlinks.sh -not -name bin -not -name config -type f`; do
 filename=`basename "$file"`
 	echo "ln -s `pwd`/$filename -> ~/.$filename"
 	ln -s `pwd`/$filename ~/.$filename
@@ -11,5 +11,12 @@ for file in `find ${PWD}/bin/ -maxdepth 1 -not -name '.*' -not -name bin`; do
 	filename=`basename "$file"`
 	echo "ln -s `pwd`/bin/$filename -> ~/bin/$filename"
 	ln -s `pwd`/bin/$filename ~/bin/$filename
+done
+
+mkdir -p ~/.config/
+for file in `find ${PWD}/config/ -maxdepth 1 -not -name '.*' -not -name config`; do
+	filename=`basename "$file"`
+	echo "ln -s `pwd`/config/$filename -> ~/.config/$filename"
+	ln -s `pwd`/config/$filename ~/.config/$filename
 done
 
