a52d2d5ff4fb61df3fab3739c41bdf88c3ed5296
Raphael Nestler <raphael.nestler@gmail.com>
2011-12-15 14:55:16 +0100

    Put redundant code into function

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -6,21 +6,24 @@ filename=`basename "$file"`
 	ln -s `pwd`/$filename ~/.$filename
 done
 
-mkdir -p ~/bin/
-for file in `find ${PWD}/bin/ -maxdepth 1 -not -name '.*' -not -name bin`; do
-	filename=`basename "$file"`
-	echo "ln -s `pwd`/bin/$filename -> ~/bin/$filename"
-	ln -s `pwd`/bin/$filename ~/bin/$filename
-done
+# $1 src $2 target
+function setupFolder {
+	mkdir -p ~/$1/
+	for file in `find ${PWD}/${1}/ -maxdepth 1 -not -name '.*' -not -name ${1}`; do
+		filename=`basename "$file"`
+		if [ -e ~/$2/$filename ]; then
+			if [ -L ~/$2/$filename ]; then
+				echo "~/$2/$filename already exists"
+			else
+				echo "WARNING: ~/$2/$filename already exists and is no symlink!"
+			fi
+		else
+			echo "ln -s `pwd`/$1/$filename -> ~/$2/$filename"
+			ln -s `pwd`/$1/$filename ~/$2/$filename
+		fi
+	done
+}
 
-mkdir -p ~/.config/
-for file in `find ${PWD}/config/ -maxdepth 1 -not -name '.*' -not -name config`; do
-	filename=`basename "$file"`
-	if [ -e ~/.config/$filename ]; then
-		echo $filename already exists
-	else
-		echo "ln -s `pwd`/config/$filename -> ~/.config/$filename"
-		ln -s `pwd`/config/$filename ~/.config/$filename
-	fi
-done
+setupFolder bin bin
+setupFolder config .config
 
