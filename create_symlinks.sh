c4347d66fa66c5e072e2a67fcad0778b1ca3b0e0
Raphael Nestler <raphael.nestler@gmail.com>
2012-07-31 02:04:34 +0200

    use a function for common code

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,34 +1,30 @@
 #!/bin/sh
 
-for file in `find ${PWD} -maxdepth 1 -not -name '.*' -not -name README -not -name create_symlinks.sh -not -name bin -not -name config -type f`; do
-filename=`basename "$file"`
-	if [ -e ~/.$filename ]; then
-		if [ -L ~/.$filename ]; then
-			echo "~/.$filename already exists"
+function createSymlink {
+	if [ -e ~/$2 ]; then
+		if [ -L ~/$2 ]; then
+			echo "~/$2 already exists"
 		else
-			echo "WARNING: ~/.$filename already exists and is no symlink!"
+			echo "WARNING: ~/$2 already exists and is no symlink!"
 		fi
 	else
-		echo "ln -s `pwd`/$filename -> ~/.$filename"
-		ln -s `pwd`/$filename ~/.$filename
+		echo "ln -s `pwd`/$1 -> ~/$2"
+		ln -s `pwd`/$1 ~/$2
 	fi
+}
+
+for file in `find ${PWD} -maxdepth 1 -not -name '.*' -not -name README -not -name create_symlinks.sh -not -name bin -not -name config -type f`; do
+filename=`basename "$file"`
+	createSymlink $filename .$filename
 done
+
 
 # $1 src $2 target
 function setupFolder {
 	mkdir -p ~/$2/
 	for file in `find ${PWD}/${1}/ -maxdepth 1 -not -name '.*' -not -name ${1}`; do
 		filename=`basename "$file"`
-		if [ -e ~/$2/$filename ]; then
-			if [ -L ~/$2/$filename ]; then
-				echo "~/$2/$filename already exists"
-			else
-				echo "WARNING: ~/$2/$filename already exists and is no symlink!"
-			fi
-		else
-			echo "ln -s `pwd`/$1/$filename -> ~/$2/$filename"
-			ln -s `pwd`/$1/$filename ~/$2/$filename
-		fi
+		createSymlink "$1/$filename" "$2/$filename"
 	done
 }
 
