ff6fdc808b528eeca958e73fd9db257250eb46b8
Raphael Nestler <raphael.nestler@gmail.com>
2021-11-09 19:57:00 +0100

    Fix a few shellcheck issues in create_symlinks.sh

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
+set -euo pipefail
+
 function createSymlink {
-	if [ -e ~/$2 ]; then
-		if [ -L ~/$2 ]; then
+	if [ -e ~/"$2" ]; then
+		if [ -L ~/"$2" ]; then
 			echo "~/$2 already exists"
 		else
 			echo "WARNING: ~/$2 already exists and is no symlink!"
 		fi
 	else
-		if [ -L ~/$2 ]; then
+		if [ -L ~/"$2" ]; then
 			echo "WARNING: ~/$2 is a broken symlink!"
 		else
-			echo "ln -s `pwd`/$1 -> ~/$2"
-			ln -s `pwd`/$1 ~/$2
+			echo "ln -s $(pwd)/$1 -> ~/$2"
+			ln -s "$(pwd)/$1" ~/"$2"
 		fi
 	fi
 }
 
 for file in `find ${PWD} -maxdepth 1 -not -name '.*' -not -name README -not -name create_symlinks.sh -not -name bin -not -name config -type f`; do
-filename=`basename "$file"`
+	filename=`basename "$file"`
 	createSymlink $filename .$filename
 done
 
 
 # $1 src $2 target
 function setupFolder {
-	mkdir -p ~/$2/
+	mkdir -p ~/"$2"/
 	for file in `find ${PWD}/${1}/ -maxdepth 1 -not -name '.*' -not -name ${1}`; do
 		filename=`basename "$file"`
 		createSymlink "$1/$filename" "$2/$filename"
