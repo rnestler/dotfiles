d29b8965b49f8965f3d2a610c47dbd18d5bffa86
Raphael Nestler <raphael.nestler@gmail.com>
2022-02-15 16:54:45 +0100

    Whitespace cleanup in setup script

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -3,35 +3,35 @@
 set -euo pipefail
 
 function createSymlink {
-	if [ -e ~/"$2" ]; then
-		if [ -L ~/"$2" ]; then
-			echo "~/$2 already exists"
-		else
-			echo "WARNING: ~/$2 already exists and is no symlink!"
-		fi
-	else
-		if [ -L ~/"$2" ]; then
-			echo "WARNING: ~/$2 is a broken symlink!"
-		else
-			echo "ln -s $(pwd)/$1 -> ~/$2"
-			ln -s "$(pwd)/$1" ~/"$2"
-		fi
-	fi
+    if [ -e ~/"$2" ]; then
+        if [ -L ~/"$2" ]; then
+            echo "~/$2 already exists"
+        else
+            echo "WARNING: ~/$2 already exists and is no symlink!"
+        fi
+    else
+        if [ -L ~/"$2" ]; then
+            echo "WARNING: ~/$2 is a broken symlink!"
+        else
+            echo "ln -s $(pwd)/$1 -> ~/$2"
+            ln -s "$(pwd)/$1" ~/"$2"
+        fi
+    fi
 }
 
 for file in `find ${PWD} -maxdepth 1 -not -name '.*' -not -name README -not -name create_symlinks.sh -not -name bin -not -name config -type f`; do
-	filename=`basename "$file"`
-	createSymlink $filename .$filename
+    filename=`basename "$file"`
+    createSymlink $filename .$filename
 done
 
 
 # $1 src $2 target
 function setupFolder {
-	mkdir -p ~/"$2"/
-	for file in `find ${PWD}/${1}/ -maxdepth 1 -not -name '.*' -not -name ${1}`; do
-		filename=`basename "$file"`
-		createSymlink "$1/$filename" "$2/$filename"
-	done
+    mkdir -p ~/"$2"/
+    for file in `find ${PWD}/${1}/ -maxdepth 1 -not -name '.*' -not -name ${1}`; do
+        filename=`basename "$file"`
+        createSymlink "$1/$filename" "$2/$filename"
+    done
 }
 
 setupFolder bin bin
