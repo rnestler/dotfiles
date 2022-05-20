daef2c30c7dcd698e93f8771504e822b74fe06e5
Raphael Nestler <raphael.nestler@gmail.com>
2022-05-20 12:04:08 +0200

    move-workspaces: Support work and home office configs

--- bin/move-workspaces.sh
+++ bin/move-workspaces.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-left=DP-3
-right=DP-4
+set -eo pipefail
 
-current_workspace=5
+current_workspace=$(swaymsg -t get_workspaces|jq '.[]|select(.focused) | .name')
 
-for i in 1 2 3 4; do
-	swaymsg workspace $i && swaymsg move workspace to output $left; swaymsg workspace $current_workspace
-done
+case $1 in
+    work)
+        output=DP-4
+        for i in 3 4 5 6 7 8 9 10; do
+            swaymsg workspace $i && swaymsg "move workspace to output $output"; swaymsg workspace "$current_workspace"
+        done
+        ;;
+    home)
+        left=DP-3
+        right=DP-4
 
-for i in 5 6 7 8; do
-	swaymsg workspace $i && swaymsg move workspace to output $right; swaymsg workspace $current_workspace
-done
+        for i in 1 2 3 4; do
+            swaymsg workspace $i && swaymsg move workspace to output $left; swaymsg workspace "$current_workspace"
+        done
+
+        for i in 5 6 7 8; do
+            swaymsg workspace $i && swaymsg move workspace to output $right; swaymsg workspace "$current_workspace"
+        done
+        ;;
+    *)
+        echo "usage $0 [work|home]"
+esac
