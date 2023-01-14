d10f5ed96e214fdb9028305d49c8c23ef370449f
Raphael Nestler <raphael.nestler@gmail.com>
2023-01-14 18:32:52 +0100

    Add script to set configurations with gsettings

---
+++ set_gsettings.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Set terminal for "open in terminal"
+# https://askubuntu.com/questions/944682/nemo-open-in-terminal-doesnt-work-in-ubuntu-mate-16-04
+gsettings set org.cinnamon.desktop.default-applications.terminal exec alacritty
